f4fa5cdfbbf75c737a59cbe81e0d8508f8a6ff9f
Nathan Adelarpe <Nathan Adelarpe@HZ114825.H012108.NET>
2022-12-05 13:58:31 +0100

    primerasion prueba

---
+++ src/gestorusuarios/Main.java
@@ -0,0 +1,20 @@
+
+package gestorusuarios;
+
+import java.io.FileNotFoundException;
+
+/**
+ * @author Eñaut Agirre
+ *
+ */
+public class Main {
+	/**
+	 * @param args
+	 * @throws FileNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		GestorUsuariosApp gua = new GestorUsuariosApp();
+		gua.run();
+	}
+
+}
